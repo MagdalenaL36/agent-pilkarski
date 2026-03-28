dea87b5cffa42b12eb0a457ef535f4f998e826e8
MagdalenaL36 <hawciu36@gmail.com>
2026-03-28 14:00:46 +0000

    config.py

--- config.py
+++ config.py
@@ -4,11 +4,8 @@
 
 FOOTBALL_DATA_API_KEY = "eefa8cd3902943429cdca38f0e48e866"
 ODDS_API_KEY          = "7f81108b160f6a524bdc339690adae05"
-
-# Gmail — alerty
-GMAIL_NADAWCA    = "neo1993@gmail.com"
-GMAIL_HASLO_APP  = "kybu rnrn tktx oepg"   # NIE zwykłe hasło — patrz README
-GMAIL_ODBIORCA   = "neo1993@gmail.com"
+TELEGRAM_BOT_TOKEN = "8428672896:AAEZS-L3S0e2TyfdVT_c9CeDU1u1mpS6g64"
+TELEGRAM_CHAT_ID   = "8740122975"
 
 # Ligi do śledzenia
 COMPETITIONS = ["PL", "BL1", "SA", "PD", "FL1", "PPL"]
